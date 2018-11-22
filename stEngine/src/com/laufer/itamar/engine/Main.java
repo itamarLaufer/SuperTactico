ffdcb89ff982a60923465bb95e04a67aaec79aa4
itamar <itamarlaufer1@gmail.com>
2018-11-22 16:16:09 +0200

    use less throws decleration

--- stEngine/src/com/laufer/itamar/engine/Main.java
+++ stEngine/src/com/laufer/itamar/engine/Main.java
@@ -2,11 +2,10 @@ package com.laufer.itamar.engine;
 
 import com.laufer.itamar.communication.server.GameServer;
 
-import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 	// write your code here
         //Todo do some tests here
         //Todo handle die of loader with safe boat
