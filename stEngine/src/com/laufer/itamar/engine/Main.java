c64e7f763b451f5a0e8edf942eabfe023b018491
itamar <itamarlaufer1@gmail.com>
2018-11-22 13:56:17 +0200

    debug the last commit using method that prings board

--- stEngine/src/com/laufer/itamar/engine/Main.java
+++ stEngine/src/com/laufer/itamar/engine/Main.java
@@ -10,8 +10,15 @@ public class Main {
 	// write your code here
         //Todo do some tests here
         //Todo handle die of loader with safe boat
-        //GameServer.runServer();
         GameServer server = new GameServer();
         server.runServer();
+    }
+    public static void printBoard(SuperTacticoGame game){
+        for(int i=0;i<20;i++){
+            for(int j=0;j<20;j++){
+                System.out.print(game.getLocTypeInLocation(new Location(i, j)));
+            }
+            System.out.println();
+        }
     }
 }
