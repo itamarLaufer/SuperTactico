c9895767bdb52e194ed870f6b6b878443d673aee
itamar <itamarlaufer1@gmail.com>
2018-11-22 14:12:34 +0200

    implement method for fliping a matrics in order to send for each player client the board in the same look vision

---
+++ stEngine/src/com/laufer/itamar/engine/EngineUtils.java
@@ -0,0 +1,22 @@
+package com.laufer.itamar.engine;
+
+public class EngineUtils {
+    /**
+     * Flips to given array in the y axis
+     * [[1, 2, 3]] -> [[1,2,3]]
+     * [[1, 2], -> [[3, 4],
+     * [3, 4]]      [1, 2]]
+     * @param arr the array to flip
+     */
+    public static <T> void flipY(T[][]arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(i < arr.length / 2){
+                    T tmp = arr[i][j];
+                    arr[i][j] = arr[arr.length - i - 1][j];
+                    arr[arr.length - i - 1][j] = tmp;
+                }
+            }
+        }
+    }
+}
--- stEngine/src/com/laufer/itamar/engine/Main.java
+++ stEngine/src/com/laufer/itamar/engine/Main.java
@@ -21,4 +21,12 @@ public class Main {
             System.out.println();
         }
     }
+    public static<T> void printArr(T[][]arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j].toString() + ',');
+            }
+            System.out.println();
+        }
+    }
 }
