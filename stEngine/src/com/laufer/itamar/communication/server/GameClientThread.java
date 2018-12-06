b5c541ed0d4a6f572f30b425f8b582e1a27e659a
itamar <itamarlaufer1@gmail.com>
2018-12-06 17:17:27 +0200

    start handle execute order

--- stEngine/src/com/laufer/itamar/communication/server/GameClientThread.java
+++ stEngine/src/com/laufer/itamar/communication/server/GameClientThread.java
@@ -3,11 +3,14 @@ package com.laufer.itamar.communication.server;
 
 import com.laufer.itamar.engine.Pieces.Piece;
 import com.laufer.itamar.engine.SuperTacticoGame;
+import com.laufer.itamar.engine.orders.MoveOrder;
+import com.laufer.itamar.engine.orders.Order;
 import org.json.simple.JSONObject;
 
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.List;
 
 public class GameClientThread implements Runnable {
     private PlayerClient client;
@@ -25,52 +28,87 @@ public class GameClientThread implements Runnable {
         JSONObject update = new JSONObject();
         update.put("board", game.getBoardAsJson());
         update.put("pieces", JsonUtils.listToJsonArray(game.getAllLivingPieces()));
-        ServerUtils.send(client.getSocket(), 4 + "_" + update.toJSONString());
+        ServerUtils.send(client.getSocket(), "4_" + update.toJSONString());
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getSocket().getInputStream()));
             String inputLine;
             String name;
             while ((inputLine = in.readLine()) != null) {
                 String[]args = inputLine.split("_");
-                if(args[0].equals("1")){
-                    if(game.isFake()) {
-                        if(args.length == 2){
-                            name = args[1];
-                            client.setName(name);
-                            server.joinAGame(client);
-                        }
-                        else
-                            ServerUtils.send(client.getSocket(), "0_" + "invalid request");
-                    }
-                    else
-                        ServerUtils.send(client.getSocket(), "You are already in a game");
+                if(args.length == 0) {
+                    ServerUtils.send(client.getSocket(), "0_" + "invalid request");
+                    continue;
                 }
-                else if(args[0].equals("3")){
-                    if(args.length == 2){
-                        int actorId;
-                        try{
-                        actorId = Integer.parseInt(args[1]);}
-                        catch (NumberFormatException e){
+                switch (args[0]) {
+                    case "1":
+                        if (game.isFake()) {
+                            if (args.length == 2) {
+                                name = args[1];
+                                client.setName(name);
+                                server.joinAGame(client);
+                            } else
+                                ServerUtils.send(client.getSocket(), "0_" + "invalid request");
+                        } else
+                            ServerUtils.send(client.getSocket(), "You are already in a game");
+                        break;
+                    case "3":
+                        if (args.length == 2) {
+                            int actorId;
+                            try {
+                                actorId = Integer.parseInt(args[1]);
+                            } catch (NumberFormatException e) {
+                                ServerUtils.send(client.getSocket(), "0_" + "invalid request");
+                                continue;
+                            }
+                            Piece actor = game.getPieceById(actorId);
+                            if (actor != null) {
+                                if (!game.isFake()) {
+                                    ServerUtils.send(client.getSocket(), "5_" + JsonUtils.listToJsonArray(actor.getPossibleOrders()));
+                                } else {
+                                    ServerUtils.send(client.getSocket(), "5_" + JsonUtils.listToJsonArray(actor.getLocateOrders()));
+                                }
+                            } else
+                                ServerUtils.send(client.getSocket(), "0_" + "invalid id");
+                        } else
                             ServerUtils.send(client.getSocket(), "0_" + "invalid request");
-                            continue;
-                        }
-                        Piece actor = game.getPieceById(actorId);
-                        if(actor != null){
-                            if(!game.isFake()){
-                                ServerUtils.send(client.getSocket(), "5_" + JsonUtils.listToJsonArray(actor.getPossibleOrders()));
+                        break;
+                    case "2":
+                        if (args.length == 3) {
+                            int actorId;
+                            int orderId;
+                            try {
+                                actorId = Integer.parseInt(args[1]);
+                                orderId = Integer.parseInt(args[2]);
+                            } catch (NumberFormatException e) {
+                                ServerUtils.send(client.getSocket(), "0_" + "invalid request");
+                                continue;
                             }
-                            else{
-                                ServerUtils.send(client.getSocket(), "5_" + JsonUtils.listToJsonArray(actor.getLocateOrders()));
-                            }
-                        }
-                        else
-                            ServerUtils.send(client.getSocket(), "0_" + "invalid id");
-                    }
-                    else
-                        ServerUtils.send(client.getSocket(), "0_" + "invalid request");
+                            Piece actor = game.getPieceById(actorId);
+                            if (actor != null) {
+                                if (!game.isFake()) {
+                                    //Todo validate, Execute the order, update all players, move turn to the next player
+                                } else {
+                                    List<MoveOrder> orders = actor.getLocateOrders();
+                                    if (orderId < orders.size() && orderId >= 0) {
+                                        System.out.println("banana");
+                                        Piece[] change = orders.get(actorId).execute();
+                                        update = new JSONObject();
+                                        update.put("pieces", JsonUtils.arrayToJsonArray(change));
+                                        //Todo maybe another data should be added like battle results, safe boat etc.
+                                        ServerUtils.send(client.getSocket(), "5_" + update.toJSONString());
+                                    } else
+                                        ServerUtils.send(client.getSocket(), "0_" + "invalid request");
+
+                                }
+                            } else
+                                ServerUtils.send(client.getSocket(), "0_" + "invalid id");
+                        } else
+                            ServerUtils.send(client.getSocket(), "0_" + "invalid request");
+                        break;
+                    default:
+                        ServerUtils.send(client.getSocket(), "0_" + "wrong request");
+                        break;
                 }
-                else
-                    ServerUtils.send(client.getSocket(), "0_" + "wrong request");
                 }
         } catch (SocketException e) { //client has disconnected
             System.out.println("client has disconnected " + client.getSocket());
