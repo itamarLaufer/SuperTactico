83fd63d1c8e641f31123a4d596a5d1275a7e792a
itamar <itamarlaufer1@gmail.com>
2019-01-22 21:34:48 +0200

    refactor

--- stEngine/src/com/laufer/itamar/communication/server/GameClientThread.java
+++ stEngine/src/com/laufer/itamar/communication/server/GameClientThread.java
@@ -5,11 +5,9 @@ import java.net.Socket;
 
 public class GameClientThread implements Runnable {
     private PlayerClient client;
-    private final GameServer server;
 
     public GameClientThread(GameServer server, Socket clientSocket) {
         this.client = new PlayerClient(clientSocket, server);
-        this.server = server;
     }
 
     public void run() {
