5bd611b41e3ff395d7f2b140d6c2815bcf7c6036
itamar <itamarlaufer1@gmail.com>
2019-05-12 22:22:17 +0300

    plane cannot be located in the beginning on land

--- stEngine/src/main/java/com/laufer/itamar/engine/Pieces/Plane.java
+++ stEngine/src/main/java/com/laufer/itamar/engine/Pieces/Plane.java
@@ -3,6 +3,10 @@ package com.laufer.itamar.engine.Pieces;
 import com.laufer.itamar.engine.*;
 import com.laufer.itamar.engine.Loads.Loads;
 import com.laufer.itamar.engine.Movings.MultipleStepsMove;
+import com.laufer.itamar.engine.orders.MoveOrder;
+
+import java.util.Iterator;
+import java.util.List;
 
 public abstract class Plane extends Piece {
     public Plane(SuperTacticoGame game, Player owner, Location location, Loads loads, int id) {
@@ -19,5 +23,12 @@ public abstract class Plane extends Piece {
     public Boolean attack(Bomb bomb){
         die();
         return false;
+    }
+
+    @Override
+    public List<MoveOrder> getLocateOrders() {
+        List<MoveOrder> res = super.getLocateOrders();
+        res.removeIf(moveOrder -> game.getLocTypeInLocation(moveOrder.getLocation()) == LocType.LAND);
+        return res;
     }
 }
