90887a276345e609ae81d081f44d87d58b02bc7b
itamar <itamarlaufer1@gmail.com>
2019-06-04 16:51:33 +0300

    update attack order according to last commit, close #33

--- stEngine/src/main/java/com/laufer/itamar/engine/BattleResult.java
+++ stEngine/src/main/java/com/laufer/itamar/engine/BattleResult.java
@@ -1,5 +1,14 @@
 package com.laufer.itamar.engine;
 
 public enum BattleResult {
-    VICTORY, DEFEAT, TIE
+    VICTORY, DEFEAT, TIE;
+
+    public BattleResult reverse(){
+        switch (this){
+            case VICTORY: return DEFEAT;
+            case DEFEAT: return VICTORY;
+            case TIE: return TIE;
+            default: return null;
+        }
+    }
 }
